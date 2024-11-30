5ee2813e4a08398747b4f59f7fc44762762ba07a
saintbassanaga <saintbassanaga01@gmail.com>
2024-11-30 04:19:31 +0100

    chore: Adding template

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,6 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('forgot-password/', auth_views.PasswordResetView.as_view(), name='forgot_password'),
